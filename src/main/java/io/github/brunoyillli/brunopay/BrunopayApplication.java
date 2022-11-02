26f363f40da0b3a5fc298fdfb75ac117b34830d6
Bruno Mendes <wolfteam1910@gmail.com>
2022-11-01 21:31:48 -0300

    iniciando projeto

---
+++ src/main/java/io/github/brunoyillli/brunopay/BrunopayApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.brunopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunopayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/brunoyillli/brunopay/BrunopayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.brunopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
